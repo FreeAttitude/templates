28fe00dc08c45212cf137e1923db478061c62816
FreeAttitude <136422200+FreeAttitude@users.noreply.github.com>
2023-12-24 10:12:02 +0100

    Delete Pier_Restaurant directory

--- Pier_Restaurant/js/setFullHeigth.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelectorAll('.full_height');
-
-    for (let i = 0; i < element.length; i++) {
-        element[i].style.height = `${window.innerHeight}px`;
-    }
-})
